b74b9b0cc47332de9bd89d8450275bb3cf7ee11a
Thien Vo <vohuythien@utexas.edu>
2016-06-07 01:25:25 -0400

    Turned book-list.js into container and implemented connection between redux state and component

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'; //Pull off single property rather than as an object with those properties
 
-export default class BookList extends Component {
+
+//Want to use container when you are trying to connect redux state/data to a view/component
+class BookList extends Component {
     render() {
         return (
             <ul className="list-group col-sm-4">
                 {this.renderList()}
             </ul>
-
 
 
             );
@@ -21,3 +23,14 @@ export default class BookList extends Component {
         });
     }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props inside of BookList
+  // Usually returning an object
+  return {
+    books: state.books
+  };
+}
+
+//Need to export the container rather than the BookList Component
+export default connect(mapStateToProps)(BookList);
